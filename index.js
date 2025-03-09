3b97b16ea684f944fa29bdc031eb3fa151db4cba
Harsh Sharma <hsharma4090@gmail.com>
2025-03-09 20:06:20 +0530

    ..

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ const { connectToMongoDB } = require("./connect");
 const URL = require("./models/url");
 const path = require("path");
 const staticRoute = require("./routes/staticRouter")
-
+const userRoute = require("./routes/user");
+const cookieParser = require("cookie-parser");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth")
 
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB("mongodb://localhost:27017/short-url")
     .then(() => console.log("mongodb connected"));
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/url", urlRoute);
-app.use("/", staticRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
+
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
@@ -27,13 +32,13 @@ app.get("/url/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
-        shortId
-    },
-     {
-        $push: {
-            visitHistory: { timestamp: Date.now() }
-        }
-    })
+            shortId
+        },
+        {
+            $push: {
+                visitHistory: { timestamp: Date.now() }
+            }
+        })
     res.redirect(entry.redirectURL);
 })
 
